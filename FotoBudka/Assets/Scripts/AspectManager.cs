fd62ae665091e3834f84b6895fb29a3eba5666b6
jacniluk <jacniluk@wp.pl>
2021-12-17 09:23:09 +0100

    Przemieszczanie kamery

--- FotoBudka/Assets/Scripts/AspectManager.cs
+++ FotoBudka/Assets/Scripts/AspectManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AspectManager : MonoBehaviour
 {
     [Header("Settings")]
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float minZoom;
     [SerializeField] private float maxZoom;
     [SerializeField] private float zoomStep;
@@ -22,37 +23,55 @@ public class AspectManager : MonoBehaviour
 
     private void Update()
     {
-        UpdateRotation();
+        UpdateRotationAndPosition();
         UpdateZoom();
     }
 
-    private void UpdateRotation()
+    private void UpdateRotationAndPosition()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
             wasClickedOnUi = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
         }
 
-        if (Input.GetMouseButton(0) && wasClickedOnUi == false)
+        if ((Input.GetMouseButton(0) || Input.GetMouseButton(1)) && wasClickedOnUi == false)
         {
             mouseOffset = Input.mousePosition - lastMousePosition;
-            float angle = Vector3.Dot(mouseOffset, Camera.main.transform.right);
-            if (Vector3.Dot(modelTransform.up, Vector3.up) >= 0)
+            if (Input.GetMouseButton(0))
             {
-                angle = -angle;
+                UpdateRotation();
             }
-            modelTransform.Rotate(modelTransform.up, angle, Space.World);
-            modelTransform.Rotate(Camera.main.transform.right, Vector3.Dot(mouseOffset, Camera.main.transform.up), Space.World);
+            else
+            {
+                UpdatePosition();
+            }
         }
 
         lastMousePosition = Input.mousePosition;
+    }
+
+    private void UpdateRotation()
+    {
+        float angle = Vector3.Dot(mouseOffset, Camera.main.transform.right);
+        if (Vector3.Dot(modelTransform.up, Vector3.up) >= 0)
+        {
+            angle = -angle;
+        }
+        modelTransform.Rotate(modelTransform.up, angle, Space.World);
+        modelTransform.Rotate(Camera.main.transform.right, Vector3.Dot(mouseOffset, Camera.main.transform.up), Space.World);
+    }
+
+    private void UpdatePosition()
+    {
+        Camera.main.transform.position -= mouseOffset * moveSpeed;
     }
 
     private void UpdateZoom()
     {
         if (Input.mouseScrollDelta.y != 0.0f)
         {
-            Camera.main.transform.position = new Vector3(0.0f, 0.0f, Mathf.Clamp(Camera.main.transform.position.z + Input.mouseScrollDelta.y * zoomStep, minZoom, maxZoom));
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y,
+                Mathf.Clamp(Camera.main.transform.position.z + Input.mouseScrollDelta.y * zoomStep, minZoom, maxZoom));
         }
     }
 
